355b67c578eea62d4caff155334762a30e26bd9d
Leonardo Diniz <work.leoaugusto@gmail.com>
2026-07-08 01:49:02 -0300

    chore: scaffold bun monorepo

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+import js from "@eslint/js";
+import ts from "typescript-eslint";
+import globals from "globals";
+
+export default [
+  { ignores: ["**/dist/**", "**/node_modules/**"] },
+  js.configs.recommended,
+  ...ts.configs.recommended,
+  {
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
+];
